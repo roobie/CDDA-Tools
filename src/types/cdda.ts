c96b7fbe6eaf8734e9301130b60ed6e839a26f3f
BR <bjorn@britkab.se>
2025-11-07 19:45:04 +0100

    improve

--- src/types/cdda.ts
+++ src/types/cdda.ts
@@ -1,18 +1,25 @@
-import { Type, type Static } from "@sinclair/typebox";
+import { Type as T, type Static } from "@sinclair/typebox";
 import { TypeCompiler } from "@sinclair/typebox/compiler";
 
-export const MathSchema = Type.Object({
-  math: Type.Union([Type.Array(Type.String()), Type.String()]),
+export const MathSchema = T.Object({
+  math: T.Union([T.Array(T.String()), T.String()]),
+});
+export const OneInChanceSchema = T.Object({
+  one_in_chance: T.Number(),
 });
 
-export const LeafEocConditionSchema = Type.Union([
-  Type.String(),
-  Type.Object({
-    math: Type.Optional(MathSchema.properties.math),
-    one_in_chance: Type.Optional(Type.Number()),
-    u_has_trait: Type.Optional(Type.String()),
-    u_has_item: Type.Optional(Type.String()),
-    not: Type.Optional(Type.String()),
+export const LeafEocConditionSchema = T.Union([
+  T.String(),
+  MathSchema,
+  OneInChanceSchema,
+  T.Object({
+    u_has_trait: T.String(),
+  }),
+  T.Object({
+    u_has_item: T.Optional(T.String()),
+  }),
+  T.Object({
+    not: T.Optional(T.String()),
   }),
 ]);
 export type LeafEocCondition = Static<typeof LeafEocConditionSchema>;
@@ -27,12 +34,12 @@ export function isLeafEocCondition(data: unknown): data is LeafEocCondition {
 //   or: Type.Optional(Type.Array(RefCompundEocConditionSchema)),
 //   not: Type.Optional(RefCompundEocConditionSchema),
 // }, { $id: "CompundEocCondition" });
-export const CompundEocConditionSchema = Type.Recursive((R) => {
+export const CompundEocConditionSchema = T.Recursive((R) => {
   // return Type.Union([Type.Object({ and: Type.Array(R) }), Type.Object({ or: Type.Array(R) }), Type.Object({ not: R })]);
-  return Type.Object({
-    and: Type.Optional(Type.Array(R)),
-    or: Type.Optional(Type.Array(R)),
-    not: Type.Optional(R),
+  return T.Object({
+    and: T.Optional(T.Array(R)),
+    or: T.Optional(T.Array(R)),
+    not: T.Optional(R),
   });
 });
 export type CompundEocCondition = Static<typeof CompundEocConditionSchema>;
@@ -46,14 +53,14 @@ export function isCompundEocCondition(data: unknown): data is CompundEocConditio
 //   ...CompundEocConditionSchema.properties,
 // });
 
-export const EocConditionSchema = Type.Recursive((R) => {
-  return Type.Union([
-    Type.String(),
-    Type.Object({
+export const EocConditionSchema = T.Recursive((R) => {
+  return T.Union([
+    T.String(),
+    T.Object({
       ...LeafEocConditionSchema.properties,
-      and: Type.Optional(Type.Array(R)),
-      or: Type.Optional(Type.Array(R)),
-      not: Type.Optional(R),
+      and: T.Optional(T.Array(R)),
+      or: T.Optional(T.Array(R)),
+      not: T.Optional(R),
     }),
   ]);
 });
@@ -64,16 +71,16 @@ export function isEocCondition(data: unknown): data is EocCondition {
   return EocConditionValidator.Check(data);
 }
 
-export const EocEffectSchema = Type.Recursive((R) => {
-  const self = Type.Union([Type.Array(R), R]);
-  return Type.Union([
+export const EocEffectSchema = T.Recursive((R) => {
+  const self = T.Union([T.Array(R), R]);
+  return T.Union([
     EocConditionSchema,
-    Type.Object({ u_message: Type.String(), type: Type.Optional(Type.String()) }),
-    Type.Object({ u_add_morale: Type.String(), bonus: Type.Number(), max_bonus: Type.Number() }),
-    Type.Object({
+    T.Object({ u_message: T.String(), type: T.Optional(T.String()) }),
+    T.Object({ u_add_morale: T.String(), bonus: T.Number(), max_bonus: T.Number() }),
+    T.Object({
       if: EocConditionSchema,
       then: self,
-      else: Type.Optional(self),
+      else: T.Optional(self),
     }),
   ]);
 });
@@ -82,7 +89,7 @@ export const EocEffectValidator = TypeCompiler.Compile(EocEffectSchema);
 export function isEocEffect(data: unknown): data is EocEffect {
   return EocEffectValidator.Check(data);
 }
-const ArrayOfEocEffectSchema = Type.Array(EocEffectSchema);
+const ArrayOfEocEffectSchema = T.Array(EocEffectSchema);
 type ArrayOfEocEffect = Static<typeof ArrayOfEocEffectSchema>;
 export const ArrayOfEocEffectValidator = TypeCompiler.Compile(ArrayOfEocEffectSchema);
 export function isArrayOfEocEffect(data: unknown): data is ArrayOfEocEffect {
@@ -133,12 +140,12 @@ if (
   console.error("EocEffect validation failed 3");
 }
 
-export const EocItemSchema = Type.Object({
-  id: Type.String(),
-  type: Type.Optional(Type.Literal("effect_on_condition")),
-  condition: Type.Optional(EocConditionSchema),
-  effect: Type.Optional(Type.Array(EocEffectSchema)),
-  false_effect: Type.Optional(Type.Array(EocEffectSchema)),
+export const EocItemSchema = T.Object({
+  id: T.String(),
+  type: T.Optional(T.Literal("effect_on_condition")),
+  condition: T.Optional(EocConditionSchema),
+  effect: T.Optional(T.Array(EocEffectSchema)),
+  false_effect: T.Optional(T.Array(EocEffectSchema)),
 });
 
 export type EocItem = Static<typeof EocItemSchema>;
